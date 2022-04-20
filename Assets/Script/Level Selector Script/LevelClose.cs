6c84b21071388b5e99b3f987b6c0e4aa63bfeb72
Gonzalo <ouribe2020@alu.uct.cl>
2022-04-19 22:31:42 -0400

    Implementacion de Selector de Nivel
    Se agregan 4 escenas para el testeo de niveles próximos y una escena con la pantalla de todos lo niveles con sus respectivas configuraciones y script

---
+++ Assets/Script/Level Selector Script/LevelClose.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelClose : MonoBehaviour
+{
+    public bool playerInZone;
+
+    public string nextLevel;
+
+    public string levelTag;
+
+    void Start() {
+        playerInZone = false;
+    }
+
+     // Update is called once per frame
+    void Update()
+    {
+          
+    }
+}
