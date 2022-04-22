27ef5ce846773d1a98fba291089ad949ceffbe42
Gonzalo <ouribe2020@alu.uct.cl>
2022-04-21 20:16:09 -0400

    Copia de test de movimiento
    Se copia la escena de test de movimiento simulando los niveles a futuro donde se comienza con el desarrollo de un menú de pausa

---
+++ Assets/Menus/Menu Pausa/MenuPausa.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuPausa : MonoBehaviour
+{
+    [SerializeField] private GameObject botonPausa;
+    [SerializeField] private GameObject menuPausa;
+
+    public void Pausa()
+    {
+        Time.timeScale = 0f;
+        botonPausa.SetActive(false);
+    }
+
+    public void Reanudar() 
+    {
+        Time.timeScale = 1f;
+        botonPausa.SetActive(true);
+        menuPausa.SetActive(false);
+    }
+}
