fc91f94b4e8d0e2a1d97264f86f810550f80bdbf
Gonzalo <ouribe2020@alu.uct.cl>
2022-05-10 17:08:19 -0400

    Room Timer

--- Assets/Menus/ScriptsMenú/Timer.cs
+++ Assets/Menus/ScriptsMenú/Timer.cs
@@ -10,49 +10,31 @@ public class Timer : MonoBehaviour
     private int minutos = 0;
     public int total = 0;
 
-    private void Awake()
-    {
+    private void Awake(){
         InvokeRepeating("Cronometro", 0f, 1f);
     }
     
-    void Cronometro()
-    {
-
+    void Cronometro(){
         segundos++;
-        if (segundos == 60)
-        {
+        if (segundos == 60){
             segundos = 0;
             minutos++;
-        }
-        
-        if(minutos >= 10)
-        {
+        }    
+        if(minutos >= 10){
             if (segundos >= 10)
-            {
                 UItexto.text = (minutos.ToString() + ":" + segundos.ToString());
-            }
             else
-            {
                 UItexto.text = (minutos.ToString() + ":0" + segundos.ToString());
-            }
         }
-        else
-        {
+        else{
             if (segundos >= 10)
-            {
                 UItexto.text = ("0" + minutos.ToString() + ":" + segundos.ToString());
-            }
             else
-            {
                 UItexto.text = ("0" + minutos.ToString() + ":0" + segundos.ToString());
-            }
-
         }
 
         total = minutos * 60 + segundos;
         Debug.Log("El jugador lleva en total "+total);
-        
         total = 0;
     }
-
 }
