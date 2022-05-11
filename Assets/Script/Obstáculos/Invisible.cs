0b0a935b39b7ee80e0914bf14e557622bc07d4e6
carayauct <caraya2020@alu.uct.cl>
2022-05-10 20:52:05 -0400

    Sala 4 nivel tutorial
    Se agregan los componentes para nuestra sala 4 de nuestro nivel introductorio.

---
+++ Assets/Script/Obstáculos/Invisible.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Invisible : MonoBehaviour
+{
+
+    public Animator animF;
+
+    protected void OnTriggerEnter2D(Collider2D collision) // Si el suelo toca al player se desactiva
+    {
+        if (collision.tag == "Player")
+            animF.Play("Fade");
+    }
+
+}
