aa4edc66105deeb38c9dbd35e44ee4ddc02ce79e
Ignacio Ortiz <iortiz2020@alu.uct.cl>
2022-05-14 17:16:27 -0400

    Opciones panel gráficos
    Se agregan algunas opciones que pertenecen al panel de opciones gráficas

---
+++ Assets/MenuOptions.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using TMPro;    
+public class MenuOptions : MonoBehaviour
+{
+    public Toggle vSyncToggle;
+
+    void Start()
+    {
+        RevisarResolucion();
+    }
+    public void Volumen(float volumen)
+    {
+
+    }
+    
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    //Por alguna raz�n no logro hacer que funcione :((
+    public void LimitarFPS()
+    {
+        if (vSyncToggle.isOn)
+        {
+            QualitySettings.vSyncCount = 1;
+        }
+        else
+        {
+            QualitySettings.vSyncCount = 0;
+        }
+    }
+
+
+    public TMP_Dropdown resolucionesDropdown;
+    Resolution[] resoluciones;                  //Arreglo que guarda todas las resoluciones de nuestro ordenador
+
+    public void RevisarResolucion()
+    {
+        resoluciones = Screen.resolutions;
+        resolucionesDropdown.ClearOptions();
+        List<string> opciones = new List<string>();
+        int resolucionActual = 0;
+
+        for (int i = 0; i < resoluciones.Length; i++)
+        {
+            string opcion = resoluciones[i].width + " x " + resoluciones[i].height;
+            opciones.Add(opcion);
+
+            if (Screen.fullScreen && resoluciones[i].width == Screen.currentResolution.width && resoluciones[i].height == Screen.currentResolution.height)
+            {
+                resolucionActual = i;
+            }
+        }
+
+        resolucionesDropdown.AddOptions(opciones);
+        resolucionesDropdown.value = resolucionActual;
+        resolucionesDropdown.RefreshShownValue();
+
+        resolucionesDropdown.value = PlayerPrefs.GetInt("numeroResolucion", 0);
+    }
+    
+    public void CambiarResolucion(int indexResolucion)
+    {
+        PlayerPrefs.SetInt("numeroResolucion", resolucionesDropdown.value);
+
+        Resolution resolucion = resoluciones[indexResolucion];
+        Screen.SetResolution(resolucion.width, resolucion.height, Screen.fullScreen);
+    }
+}
